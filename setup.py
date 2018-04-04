d0f67850fe04ad3ecc70bf040945627269810902
Chris Curvey <chris@chriscurvey.com>
2018-04-04 08:43:34 -0400

    fixed git specifier

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         'matplotlib',
         'nltk',
         'numba',
-        '-e git://github.com/transparint/numbskull@master',
+        'git://github.com/transparint/numbskull@master',
         'numpy>=1.11',
         'pandas',
         'requests',
